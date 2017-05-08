a1354473884f105dbd42d2d093366359a3bed62a
Michael Chiang <chiangshtaa@gmail.com>
2017-05-08 12:21:17 -0700

    add some and test

--- test/underbar/some.test.js
+++ test/underbar/some.test.js
@@ -3,10 +3,14 @@ const _ = require('../../underbar');
 describe('some()', () => {
   it('returns true if any number is odd', () => {
     // Your code goes here
+    const arr = [1, 2, 3, 4, 5];
+    expect(_.some(arr, num => num % 2 === 1)).toEqual(true);
   });
 
   it('returns false if no number is odd', () => {
     // Your code goes here
+    const arr = [0, 2, 4, 6, 8];
+    expect(_.some(arr, num => num % 2 === 1)).toEqual(false);
   });
 
 });

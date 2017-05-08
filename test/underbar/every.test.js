1167b4882005e9d9baa696fc7e3c6a310e723733
Michael Chiang <chiangshtaa@gmail.com>
2017-05-08 12:18:42 -0700

    add every and test

--- test/underbar/every.test.js
+++ test/underbar/every.test.js
@@ -4,14 +4,20 @@ describe('every()', () => {
   describe('processing an array of numbers', () => {
     it('will return true if no callback is supplied', () => {
       // Your code goes here
+      const arr = [1, 2, 3, 4, 5];
+      expect(_.every(arr)).toEqual(true);
     });
 
     it('returns true if all numbers in an array are odd and we test for odd numbers', () => {
       // Your code goes here
+      const arr = [1, 3, 5, 7, 9];
+      expect(_.every(arr, num => num % 2 === 1)).toEqual(true);
     });
 
     it('returns false if not all numbers in an array are odd and we test for odd numbers', () => {
       // Your code goes here
+      const arr = [1, 2, 3, 4, 5];
+      expect(_.every(arr, num => num % 2 === 1)).toEqual(false);
     });
 
   });

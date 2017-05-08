d70ede5cef99fa1a311a43334321b6b5c6470aac
Michael Chiang <chiangshtaa@gmail.com>
2017-05-08 12:28:16 -0700

    add filter and test

--- test/underbar/filter.test.js
+++ test/underbar/filter.test.js
@@ -3,9 +3,19 @@ const _ = require('../../underbar');
 describe('filter()', () => {
   it('filters an array to odd numbers', () => {
     // Your code goes here
+    const arr = [1, 2, 3, 4, 5];
+    expect(_.filter(arr, num => num % 2 === 1)).toEqual([1, 3, 5]);
   });
 
   it('filters an object to only numeric values', () => {
     // Your code goes here
+    const obj = {
+      name: 'Michael',
+      a: 1,
+      b: 2,
+      c: 3
+    };
+    const nums = _.filter(obj, element => !isNaN(element));
+    expect(nums).toEqual([1, 2, 3]);
   });
 });
